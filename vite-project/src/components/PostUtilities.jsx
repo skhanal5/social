6ef2ab9ea00733b4ccf42643d78071b973655be8
Subodh Khanal <skhanal5@terpmail.umd.edu>
2023-11-10 22:40:30 -0500

    Post mock implementation completed

---
+++ vite-project/src/components/PostUtilities.jsx
@@ -0,0 +1,21 @@
+import {
+  ArrowUpCircleIcon,
+  ArrowDownCircleIcon,
+  ChatBubbleLeftIcon,
+} from "@heroicons/react/24/outline";
+import React from "react";
+export default function PostUtilities() {
+  return (
+    <div className="post-utils">
+      <div className="post-util">
+        <ArrowUpCircleIcon className="post-util-icon"></ArrowUpCircleIcon>
+        <span className="post-util-txt">14</span>
+      </div>
+      <div className="post-util">
+        <ArrowDownCircleIcon className="post-util-icon"></ArrowDownCircleIcon>
+        <span className="post-util-txt">30</span>
+      </div>
+      <ChatBubbleLeftIcon className="post-util-icon"></ChatBubbleLeftIcon>
+    </div>
+  );
+}
